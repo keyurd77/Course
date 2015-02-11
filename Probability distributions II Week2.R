8327e36bcf46eb9fe13ecaf1eb86b93fec8984cb
unknown <keyurd@QIMR10726.adqimr.ad.lan>
2015-02-11 17:22:46 +1000

    20150211-2

--- Probability distributions II Week2.R
+++ Probability distributions II Week2.R
@@ -7,4 +7,13 @@ library(gapminder)
 ?gapminder# find more about the dataframe
 data(gapminder) # loads data
 head(gapminder)# returns first few lines of the data
+#Create a vector 'x' of the life expectancies of each 
+#country for the year 1952. Plot a histogram of these 
+#life expectancies to see the spread of the different countries.
+View(gapminder) #see the table
+p<- gapminder[gapminder$year<1953,c(1,4)] # created a vector 'p' extracted the column
+#year with values less than 1953 for all rows (as I require only 1952) and combined 
+#columns 1 (country) and 4 (lifeexpectancy)
+p # see the extracted data
+plot(p) # plot
 
