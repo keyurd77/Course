e3113996e729750687adeccdef9ff7b4051f20c6
unknown <keyurd@QIMR10726.adqimr.ad.lan>
2015-02-11 12:05:04 +1000

    Edx course

---
+++ Probability distribution.R
@@ -0,0 +1,45 @@
+dat <- read.csv("femaleMiceWeights.csv")
+dat
+dat [1:12, 2]
+dat [13:24, 2]
+mean(dat[13:24,2]) - mean(dat[1:12,2])
+s = split(dat[,2], dat[,1])
+s
+stripchart(s, vertical=TRUE, col=1:2)
+abline(h=sapply(s, mean), col=1:2)
+highfat <- s[["hf"]]
+highfat
+sample(highfat, 6)
+sample(highfat, 6, replace=TRUE)
+highfat>30
+as.numeric(highfat>30)
+sum(highfat>30)
+mean(highfat>30)
+population <- read.csv("femaleControlsPopulation.csv")
+population
+control <- sample(population[ ,1], 12)
+mean(control)
+
+# to perform test 10000 times to see if it is possible to see 3.020833 by random chance
+diff <- mean(dat[13:24,2]) - mean(dat[1:12,2])
+n<- 10000
+null <- vector("numeric", n)
+for (i in 1:n){
+  control <-sample(population[ ,1], 12)
+  treatment <-sample(population[ ,1], 12)
+  null[i] <- mean(treatment) - mean(control)
+}
+mean(null>diff) # what proportion of times where this happens
+#only 1 or 2%, this is the p value
+population
+population = population [,1]
+mean (population)
+# performing the same thing in a more concise manner
+SampleMean <- replicate(10000, mean(sample(population,12))) # assign to the name"sampleMean" 
+#the outcome of replicating
+#10000 times the mean of random variable from the dataset population (choosing 12 random values)
+head(SampleMean)
+plot(SampleMean)
+null <- replicate(10000, mean(sample(population,12)) - mean(sample(population, 12))) # to perform the same thing
+head(null)
+plot(null)
