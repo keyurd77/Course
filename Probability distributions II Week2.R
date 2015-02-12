2854ad6c4e19d9010e8a4441221ef35cd72e857c
unknown <keyurd@QIMR10726.adqimr.ad.lan>
2015-02-12 13:33:48 +1000

    2

--- Probability distributions II Week2.R
+++ Probability distributions II Week2.R
@@ -26,4 +26,17 @@ library(ggplot2)
 bp<-ggplot(x, aes(x=country, y=lifeExp)) + geom_bar(stat="identity", width=0.5)
 bp + theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5)) # just 
 #playing around with ggplot2 ;)
+mean(x$lifeExp<=40)#What is the proportion of countries in 1952 that have a life expectancy less than or equal to 40?
+#[1] 0.2887324
+#dat1952 = gapminder[ gapminder$year == 1952, ]
+#x = dat1952$lifeExp
+#mean(x <= 40)
+mean(x$lifeExp<=60) - mean(x$lifeExp<=40)#What is the proportion of countries in 1952 
+#that have a life expectancy between 40 and 60 years? Hint: this is the 
+#proportion that have a life expectancy less than or equal to 60 years,
+#minus the proportion that have a life expectancy less than or equal to 40 years.
+#[1] 0.4647887
+#dat1952 = gapminder[ gapminder$year == 1952, ]
+#x = dat1952$lifeExp
+#mean(x <= 60) - mean(x <= 40)
 
